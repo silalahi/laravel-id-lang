7916f9ee0b6f64bf8d84cb122038e2cbc94913aa
Jogi Silalahi <jogi@codigo.id>
2016-06-01 05:13:26 +0700

    Update localization

--- src/resources/lang/id/password.php
+++ src/resources/lang/id/password.php
@@ -2,8 +2,8 @@
 <?php
 return [
     'password' => 'Kata sandi harus minimal enam karakter dan cocok dengan konfirmasi.',
-    'reset'    => 'Kata sandi Anda sudah direset!',
-    'sent'     => 'Kami sudah mengirim email yang berisi tautan untuk mereset kata sandi Anda!',
+    'reset'    => 'Kata sandi Anda sudah di atur ulang!',
+    'sent'     => 'Kami sudah mengirim email yang berisi tautan untuk mengatur ulang kata sandi Anda!',
     'token'    => 'Kata sandi token pengaturan ulang tidak sah.',
     'user'     => 'Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.',
 ];
